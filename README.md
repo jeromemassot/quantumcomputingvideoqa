6df0e736c37011987d876fedc6a51792deef0ffd
Jerome Massot <jerome.massot.78@gmail.com>
2022-12-22 16:59:26 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Google and IBM Quantum Computing Videos Q&A engine
+
+Semantic Search for Google and IBM Quantum Computing videos
+
+This SteamLit webapp is backended by a Pinecone index referencing Google and IBM videos transcripts.
+It gives to the users the possiblity to run semantic search on these videos transcripts.
+
+For each query proceeded, the Q&A engine returns elements of answering and the url links pointing to
+the exact moment of the videos when query answers are discussed.
+
+Jerome MASSOT
---
+++ streamlit_app.py
@@ -0,0 +1,156 @@
+ # Copyright : Petroglyphs NLP Consulting
+ # author: Jerome Massot (jerome.massot.78@gmail.com)
+
+from sentence_transformers import SentenceTransformer
+from nltk.tokenize import sent_tokenize
+from transformers import pipeline
+
+from pinecone import PineconeProtocolError
+import pinecone
+
+import streamlit as st
+import pandas as pd
+
+
+@st.experimental_singleton
+def init_pinecone():
+    """
+    Init the pinecone index with api key which is treated as secret
+    when the app is deployed on Streamlit Cloud.
+    :return: pinecone index object
+    """
+    api_key = st.secrets['API_KEY']
+    pinecone.init(api_key=api_key, environment='us-west1-gcp')
+    return pinecone.Index('video-search')
+    
+
+@st.experimental_singleton
+def init_retriever():
+    """
+    Return the LLM indicated in the code
+    :return: Language Model object
+    """
+    return SentenceTransformer('multi-qa-mpnet-base-dot-v1')
+
+
+@st.experimental_singleton
+def load_qa_pipeline():
+    """
+    Load the HuggingFace pipeline for Question-Answering
+    :return: HuggingFace pipeline object
+    """
+    return pipeline("question-answering", model='deepset/roberta-base-squad2')
+
+
+def reconstruct_answered_context(index, query, top_k=3, origin=['Google', 'IBM']):
+    """
+    Find the contexts used for the Q&A engine using the query
+    :param query: question asked by the user
+    :param top_k: number of contexts to return from the index
+    :param origin: origin of the videos to be used as filter
+    :return: list of sentences as string objects
+    """
+ 
+    # embed the query
+    xq = retriever.encode([query]).tolist()
+    
+    # search the contexts
+    try:
+        xc = index.query(
+            xq, 
+            top_k=top_k,
+            filter={'origin': {"$in": origin}},
+            include_metadata=True
+        )
+    except PineconeProtocolError:
+        index = init_pinecone()
+        xc = index.query(
+            xq, 
+            top_k=top_k,
+            filter={'origin': {"$in": origin}},
+            include_metadata=True
+        )
+    
+    # reconstruct the contexts and find the answers
+    returned_contexts_answers = list()
+    for context in xc['matches']:
+        text_context = context['metadata']['text']
+
+        returned_contexts_answers.append({
+            'title': context['metadata']['title'],
+            'text': text_context,
+            'origin': context['metadata']['origin'],
+            'url': context['metadata']['url'], 
+            'keywords': context['metadata']['keywords'],
+            'start': context['metadata']['start_second']
+        })
+                
+    return returned_contexts_answers
+
+# interface
+
+st.image("./decorations/logo.png", width=200)
+
+st.title("Google and IBM Quantum Computing Videos Q&A Engine")
+st.subheader("Explore knowledge from Google & IBM YouTube videos")
+
+# Index and Retriever model setup
+index = init_pinecone()
+retriever = init_retriever()
+#question_answerer = load_qa_pipeline()
+
+query = st.text_input("Question:", help="enter your question here")
+filter_origin = st.multiselect(label="Content Origin", options=['Google', 'IBM'], help="Search from Google and/or IBM")
+top_k = st.number_input("Nb of returned context:", 1, 3, help="Top 3 ranking contexts maximum")
+search = st.button("Search")
+
+if search and query != "":
+    returned_contexts_answers = list(reconstruct_answered_context(index, query, top_k, filter_origin))
+
+    if returned_contexts_answers and len(returned_contexts_answers)>0:
+        merged_text = ''
+        columns = st.columns(len(returned_contexts_answers))
+        for i, col in enumerate(columns):
+            with col:
+                title = returned_contexts_answers[i]['title']
+                text = returned_contexts_answers[i]['text']
+                topics = returned_contexts_answers[i]['keywords']
+                url = returned_contexts_answers[i]['url']
+                origin = returned_contexts_answers[i]['origin']
+                start = int(returned_contexts_answers[i]['start'])
+
+                st.markdown(f'<div style="text-align: center;"><b>{title}</b></div>', unsafe_allow_html=True)
+                displayed_text = '... ' + text + '...'
+                st.caption(f'<div style="text-align: justify;">{displayed_text}</div>', unsafe_allow_html=True)
+                st.caption(' ')
+                st.video(url, start_time=start)
+                st.markdown(f"**Topic**: {', '.join(topics)}")
+                st.markdown(f"**Origin**: {origin}")
+
+                merged_text += ' ' + text
+
+        #answer = question_answerer(question=query, context=merged_text)['answer']
+        #st.info(f'A possible answer to your question is: **{answer}**')
+
+    else:
+        st.warning("Nothing found, sorry...")
+
+
+st.warning(
+    """
+    Disclaimer: the content shown in this page is the exclusive property of Google or IBM. 
+    """
+)
+
+bottom_column_1, bottom_column_2 = st.columns([1, 9])
+with bottom_column_1:
+    st.image("./decorations/logo-petroglyphs.jpg", width=60)
+
+with bottom_column_2:
+    st.caption(
+        """
+        If you are interested by adding similar Semantic Search Engine 
+        to your content, please contact Petroglyphs NLP Consulting 
+        (jerome.massot.78@gmail.com)
+        """
+    )
